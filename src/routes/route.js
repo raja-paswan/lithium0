d8368a2fda2b92930519a665f7f80acd092ed143
raja <rkpraja9060@gmail.com>
2022-10-18 00:48:04 +0530

    assigment

--- src/routes/route.js
+++ src/routes/route.js
@@ -15,6 +15,53 @@ router.get('/students', function (req, res){
 })
 
 
+router.get('/profile', function (req, res){
+    console.log("The path params in the request are : ", req.params)
+    let students = "hi my name is raja"
+    res.send(students)
+})
+
+router.get('/movies', function (req, res){
+    console.log("list of movies", req.params)
+    let listofmovies = ['rang de basanti','the sining','lord of ring','batman begins'];
+    res.send(listofmovies)
+})
+// router.get('/movie/:index', function (req, res){
+//     console.log("list of movies", req.params)
+//     let listofmovie=['rang de basanti','the sining','lord of ring','batman begins'];
+//     const index=req.params.index;
+//     res.send(listofmovie)
+// })
+
+// router.get('/movie/:indexNumber', (req,res) => {
+//     let index = req.params.indexNumber
+//     if(index>movies.length){
+//         res.send('Please use a valid index')
+//     }
+//     else{
+//         res.send(movies[index])
+//     }
+// })
+//2and 3////////////////////////////////////////////////////////////////////
+router.get('/movies/:indexNumber',(req,res)=>{
+    let index=req.params.indexNumber
+    let returnn=''
+    let movies=['rang de basanti','the sining','lord of ring','batman begins'];
+    returnn=(movies.length>index)?movies[index]:"error : wrong"
+    res.send(returnn)
+})
+///////////////////////////////3///////////////////////////////////
+router.get('/films',function(req, res){
+    let films=[ {
+               'id':1,
+               'name':'the shing'},
+               {
+                'id':2,
+                'name':'ince'
+               },]
+               res.send(films)
+            });
+
 // Example 1 for path params
 router.get('/students/:studentName', function(req, res){
     // ':' denotes that the following part of route is a variable
@@ -23,6 +70,7 @@ router.get('/students/:studentName', function(req, res){
     // The object contain key value pairs
     // key is the variable in the route
     // value is whatever dynamic value sent in the request url
+    
     let myParams = req.params
 
     // params attribute is fixed in a request object
