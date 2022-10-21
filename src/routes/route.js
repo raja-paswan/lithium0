7765271cfc3ce3be9b6cc8b2074cf4efb418e878
raja <rkpraja9060@gmail.com>
2022-10-21 09:24:17 +0530

    quaryvspath

--- src/routes/route.js
+++ src/routes/route.js
@@ -1,3 +1,4 @@
+const { Router } = require('express');
 const express = require('express');
 const router = express.Router();
 
@@ -159,5 +160,50 @@ router.post( "/post-query-2", function (req, res){
     res.send( {data: finalArr , status: true})
 })
 
+let persons = [
+  {
+    name: "PK",
+    age: 10,
+    votingStatus: false,
+  },
+  {
+    name: "SK",
+    age: 20,
+    votingStatus: false,
+  },
+  {
+    name: "AA",
+    age: 70,
+    votingStatus: false,
+  },
+  {
+    name: "SC",
+    age: 5,
+    votingStatus: false,
+  },
+  {
+    name: "HO",
+    age: 40,
+    votingStatus: false,
+  },
+];
+
+router.post("/vote", function (req, res) {
+  let vote = req.query.age;
+  if (vote < 18) return res.send({
+     status: "You are not eligible For Voting" });
+  let final = [];
+
+  for (i of persons) {
+    if (i.age >= vote) {
+      i.votingStatus = "true";
+      final.push(i);
+    }
+  }
+  console.log(final);
+  return res.send({ Updated: final });
+});
+
+module.exports = router;
 
 module.exports = router;
